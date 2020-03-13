56a81ef9497f984bbdec5d51689440d3f94d6b15
yds <yaodisongmail@163.com>
2020-03-13 17:38:16 +0800

    2020.3.13 threadtest

--- ppstest/src/com/pps/test/ThreadTest.java
+++ ppstest/src/com/pps/test/ThreadTest.java
@@ -3,12 +3,13 @@ package com.pps.test;
 import java.util.concurrent.FutureTask;
 
 public class ThreadTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 //        Thread t = new Thread(() -> System.out.println("Runnable线程启动"));
 //        Thread t1 = new Thread();
 //        t.start();
 //        t1.start();
         MyCallable myCallable = new MyCallable();
+//        myCallable.call();//和thread.run()类似只是跑一遍方法，不是真正意义的并发
         FutureTask futureTask = new FutureTask(myCallable);
         Thread thread = new Thread(futureTask);
         thread.start();
